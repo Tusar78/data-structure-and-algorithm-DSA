b24d6e695ff0c3dc8cdb9c8cf098068e8c096247
Tusar <tusarahmed141@gmail.com>
2022-07-02 16:50:31 +0600

    Big order of 1

---
+++ 01_Time_Complexity/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Example one of BIG O(1).
+int main()
+{
+  int x, y, result;
+  x = 10;
+  y = 20;
+  result = x + y;
+  printf("Your result is: %d", result);
+  return 0;
+}
