20121f06f93e4e041ea5d2ce0dcf52d6805d4e4a
Tusar <tusarahmed141@gmail.com>
2022-07-02 18:14:17 +0600

    Explore big Order of n qube notation using language js

--- 01_Time_Complexity/JavaScript/main.js
+++ 01_Time_Complexity/JavaScript/main.js
@@ -20,14 +20,49 @@
 // main();
 
 // Example three big O(n). using for loop
+// function main() {
+//   let i, n, result;
+//   n = +prompt('Enter your number', '')
+//   result = 0;
+//   for (i = 1; i <= n; i++) {
+//     result += i;    
+//   }
+//   console.log(result);;
+// }
+
+// main()
+
+// Example of big O(n_squire)
+// function main() {
+//   let i, j, n, count;
+//   n = +prompt('Enter your number: ', '');
+//   count = 0;
+//   for (i = 0; i < n; i++) {
+//     for (j = 0; j < n; j++) {
+//       count += 1;
+//     }
+//   }
+
+//   console.log(`You entered ${n} and your count value is: ${count}`);
+// }
+
+// main();
+
+// Example of big O(n_qube)
 function main() {
-  let i, n, result;
-  n = +prompt('Enter your number', '')
-  result = 0;
-  for (i = 1; i <= n; i++) {
-    result += i;    
+  let i, j, k, n, count;
+  n = +prompt('Enter your number: ', '');
+  
+  count = 0;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      for (k = 0; k < n; k++) {
+        count += 1;
+      }
+    }    
   }
-  console.log(result);;
+
+  console.log(`You entered ${n} and your count value is: ${count}`);
 }
 
 main()
