732a1affad087a0e2aa3db71f65ddeeb387f553a
Tusar <tusarahmed141@gmail.com>
2022-07-02 17:00:42 +0600

    Explore big o(n) notation

--- 01_Time_Complexity/main.c
+++ 01_Time_Complexity/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // Example one of BIG O(1).
+/*
 int main()
 {
   int x, y, result;
@@ -9,4 +10,15 @@ int main()
   result = x + y;
   printf("Your result is: %d", result);
   return 0;
+}
+*/
+
+// Example two of BIG O(n). showing shomantor dhara rules: n * ( n + 1 ) / 2
+
+int main() {
+  int n, result;
+  printf("Enter your number: ");
+  scanf("%d", &n);
+  result = (n * ( n + 1 )) / 2;
+  printf("Your result is: %d", result);
 }
